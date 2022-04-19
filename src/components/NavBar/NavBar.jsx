2e1d1434b073bc06fae47fbfbeef9463cc5a6725
Ayelen <drmelo2540@gmail.com>
2022-04-19 20:00:48 -0300

    navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css'
+import logo1 from './logo1.jpg'
+function NavBar(props) {
+    return (
+        <div className='nav'>
+            <a className='nav__brand' href="#"><img className='nav__logo' src={logo1} alt="(LOGO)" /><h1>Sahumá</h1></a>
+            <ul className='nav__list'>
+                <li><a className='nav__item' href="#">Productos</a></li>
+                <li><a className='nav__item' href="#">Nosotros</a></li>
+                <li><a className='nav__item' href="#">Contacto</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
