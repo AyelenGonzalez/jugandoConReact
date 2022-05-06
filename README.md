7d74d1bee9c1e57e30f34d1e9dd8b8515edc9343
Ayelen <drmelo2540@gmail.com>
2022-05-05 23:44:41 -0300

    PF 1 + navegación

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Utilicé 3 rutas para renderizar respectivamente la galería de productos comple
 Comencé trayendo a los componentes el array de productos desde un archivo externo con una promesa pero al encontrarme con algunos problemas decidí tener dicho array cargado directamente en los componentes inteligentes de cada ruta (itemBox e itemDetailContainer).
 
 En ambos componentes inteligentes cargué los datos de los productos a través de una promesa con setTimeOut al renderizarlo y con los parámetros necesarios para filtrar cada categoría y producto(category/Id).
+
+Utilicé además de las dependencias propias de la app, react-router-dom, bootstrap para utilizar clases y también react-bootstrap para traer componentes(card).
--- src/Pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -16,15 +16,15 @@ class Producto {
             }
         }
 const productos = [];
-productos.push(new Producto(1, 'Lavanda y Olíbano', `Especiales`,`../image/sahume2.jpeg`, 300));
-productos.push(new Producto(2, 'Naranja y Citronella', `Especiales`,`../image/sahume.jpg`, 300));
-productos.push(new Producto(3, 'Linea Natural X 3 "Rosas"', `Packs`, `../image/sahume6.jpg`, 600));
-productos.push(new Producto(4, 'Linea Natural X 4', `Packs`, `../image/sahume5.jpg`, 900));
-productos.push(new Producto(5, 'Linea Natural X 5 "Para las tardes"', `Packs`, `../image/sahume4.jpg`, 900));
-productos.push(new Producto(6, 'Linea Premium X 4 "Intenso"', `Packs`, `../image/sahume3.jpg`, 900));
-productos.push(new Producto(7, 'Linea Premium X 5 "Ruda"', `Packs`, `../image/sahume7.jpg`, 1000));
-productos.push(new Producto(8, 'Linea Premium Triple combinado x 5 "Noche"', `Combinados`, `../image/sahume8.jpg`, 1000));
-productos.push(new Producto(9, 'Linea Premium Triple combinado X 5 "Dia"', `Combinados`, `../image/sahume9.jpg`, 1000));
+productos.push(new Producto(1, 'Lavanda y Olíbano', `Especiales`,`../image/sahume2.jpeg`, 300, 10));
+productos.push(new Producto(2, 'Naranja y Citronella', `Especiales`,`../image/sahume.jpg`, 300, 10));
+productos.push(new Producto(3, 'Linea Natural X 3 "Rosas"', `Packs`, `../image/sahume6.jpg`, 600, 20));
+productos.push(new Producto(4, 'Linea Natural X 4', `Packs`, `../image/sahume5.jpg`, 900, 20));
+productos.push(new Producto(5, 'Linea Natural X 5 "Para las tardes"', `Packs`, `../image/sahume4.jpg`, 900, 20));
+productos.push(new Producto(6, 'Linea Premium X 4 "Intenso"', `Packs`, `../image/sahume3.jpg`, 900, 20));
+productos.push(new Producto(7, 'Linea Premium X 5 "Ruda"', `Packs`, `../image/sahume7.jpg`, 1000, 20));
+productos.push(new Producto(8, 'Linea Premium Triple combinado x 5 "Noche"', `Combinados`, `../image/sahume8.jpg`, 1000, 30));
+productos.push(new Producto(9, 'Linea Premium Triple combinado X 5 "Dia"', `Combinados`, `../image/sahume9.jpg`, 1000, 30));
 
 function getItem(id){
     const myPromise = new Promise ((res) =>{
